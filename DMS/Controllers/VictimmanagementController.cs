a493b4b865891b6be67c467e41bddd067258da5a
simrika dangol <simrikadangol11@gmail.com>
2024-09-16 20:01:28 +0545

    victim manageent and dashboard

---
+++ DMS/Controllers/VictimmanagementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMS.Controllers
+{
+    public class VictimmanagementController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
