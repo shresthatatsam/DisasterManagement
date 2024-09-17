375751fa9681881a951ff6ce9aaa12ebda67fdd9
Tatsam <tatsamshrestha93@gmail.com>
2024-09-16 23:24:55 -0700

    changes in disaster anagement

--- DMS/Controllers/DisastermanagementController.cs
+++ DMS/Controllers/DisastermanagementController.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Disaster_Management_system.Data.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DMS.Controllers
 {
     public class DisastermanagementController : Controller
     {
+
+        private readonly IDisasterCategory _disasterService;
+
+        // Constructor injection
+        public DisastermanagementController(IDisasterCategory disasterService)
+        {
+            _disasterService = disasterService;
+        }
+
+
         [Authorize(Roles = "Admin")]
 
         public IActionResult Index()
         {
-            return View();
+            var disasters = _disasterService.getAllDisasters();
+            return View(disasters);
         }
     }
 }
